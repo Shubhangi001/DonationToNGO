576e98f1569f739a897d1ce14e0d97629d72cad8
preeti122 <ppallavi2002@gmail.com>
2021-11-17 23:19:14 +0530

    minor change\

--- NGO_RPS/Donate/views.py
+++ NGO_RPS/Donate/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from . import models
+from . import forms, models
 # from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib import auth
 from django.contrib.auth.models import Group
+from .forms import NewDonorForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 # Create your views here.
@@ -18,6 +21,16 @@ def ngo(request):
     return render(request,'Donate/ngo.html') 
 
 def donor_signup(request):
+    if request.method == "POST":
+        form = NewDonorForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("main:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")		
+	form = NewDonorForm()
+
     return render(request, "Donate/donor_signup.html")
 
 def donor_login(request):
