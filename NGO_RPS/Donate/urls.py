e63e2676cf3e6223bd20c3302de956852e05e40e
preeti122 <ppallavi2002@gmail.com>
2021-11-17 22:50:06 +0530

    initia html pgs

--- NGO_RPS/Donate/urls.py
+++ NGO_RPS/Donate/urls.py
@@ -3,6 +3,11 @@ from . import views
 app_name='Donate'
 urlpatterns = [
         path('', views.firstpage,name='firstpage'),
+        path('donor', views.donor,name='donor'),
+        path('ngo', views.ngo,name='ngo'),
+        path('donor_signup', views.donor_signup,name='donor_signup'),
+        path('donor_login', views.donor_login,name='donor_login'),
+        path('ngo_signup', views.ngo_signup,name='ngo_signup'),
+        path('ngo_login', views.ngo_login,name='ngo_login'),
 
-  
 ]
--- NGO_RPS/Donate/views.py
+++ NGO_RPS/Donate/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 # from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from . import forms,models
-from django.contrib.auth.decorators import login_required,user_passes_test
+from . import models
+# from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib import auth
 from django.contrib.auth.models import Group
 
 
 # Create your views here.
 def firstpage(request):
-    return render(request,'firstpage.html') 
-def adminsignup_view(request):
-    form=forms.AdminSigupForm()
-    if request.method=='POST':
-        form=forms.AdminSigupForm(request.POST)
-        if form.is_valid():
-            user=form.save()
-            user.set_password(user.password)
-            user.save()
-            my_admin_group = Group.objects.get_or_create(name='NGO')
-            my_admin_group[0].user_set.add(user)
-            return HttpResponseRedirect('adminlogin')
-    return render(request,'library/adminsignup.html',{'form':form})
-    # Create your views here.
+    return render(request,'Donate/firstpage.html') 
+
+def donor(request):
+    return render(request,'Donate/donor.html') 
+
+def ngo(request):
+    return render(request,'Donate/ngo.html') 
+
+def donor_signup(request):
+    return render(request, "Donate/donor_signup.html")
+
+def donor_login(request):
+    return render(request, "Donate/donor_login.html")
+
+def ngo_signup(request):
+    return render(request, "Donate/ngo_signup.html")
+
+def ngo_login(request):
+    return render(request, "Donate/ngo_login.html")
+
+
+# def adminsignup_view(request):
+#     form=forms.AdminSigupForm()
+#     if request.method=='POST':
+#         form=forms.AdminSigupForm(request.POST)
+#         if form.is_valid():
+#             user=form.save()
+#             user.set_password(user.password)
+#             user.save()
+#             my_admin_group = Group.objects.get_or_create(name='NGO')
+#             my_admin_group[0].user_set.add(user)
+#             return HttpResponseRedirect('adminlogin')
+#     return render(request,'library/adminsignup.html',{'form':form})
