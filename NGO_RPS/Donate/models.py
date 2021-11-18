566b07c158917cb5d1b93bd926d5b315af991d70
preeti122 <ppallavi2002@gmail.com>
2021-11-18 21:37:04 +0530

    ngolist model created

--- NGO_RPS/Donate/models.py
+++ NGO_RPS/Donate/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Ngolist(models.Model):
+    ngoname = models.CharField(max_length=150)
+    mobno = models.CharField(max_length=10)
+    place = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ngoname
