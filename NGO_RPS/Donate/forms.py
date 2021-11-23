07dab3d2d8fa4367d18533facf47e5b290b2daa9
Shubhangi001 <shubhangispc@gmail.com>
2021-11-23 22:24:01 +0530

    forms

--- NGO_RPS/Donate/forms.py
+++ NGO_RPS/Donate/forms.py
@@ -1,17 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from . import models
 
-class NewDonorForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+# from . import models
+class NewNGOForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields=['first_name','last_name','username','password']
+
+
+class NewDonorForm(forms.ModelForm):
+	# email = forms.EmailField(required=True)
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields=['first_name','last_name','username','password']
 
-	def save(self, commit=True):
-		user = super(NewDonorForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
+
+	# def save(self, commit=True):
+	# 	user = super(NewDonorForm, self).save(commit=False)
+	# 	user.email = self.cleaned_data['email']
+	# 	if commit:
+	# 		user.save()
+	# 	return user
