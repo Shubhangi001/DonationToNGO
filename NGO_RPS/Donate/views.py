4cd87faffe3a8b4c55b4b6719b65a89310510453
Shubhangi001 <shubhangispc@gmail.com>
2021-11-23 22:22:45 +0530

    views

--- NGO_RPS/Donate/views.py
+++ NGO_RPS/Donate/views.py
@@ -5,50 +5,78 @@ from . import forms, models
 # from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib import auth
 from django.contrib.auth.models import Group
-from .forms import NewDonorForm
+from .forms import NewDonorForm,NewNGOForm
 from django.contrib.auth import authenticate,login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def firstpage(request):
-    return render(request,'Donate/firstpage.html')  
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('afterlogin')
+	return render(request,'Donate/firstpage.html')
+def is_ngo(user):
+    return user.groups.filter(name='NGO').exists()
+
+def ngo_signup(request):
+	form2=NewNGOForm()
+	dict2={'form2':form2}
+	if(request.method == "POST"):
+		form2 = NewNGOForm(request.POST)
+		if form2.is_valid():
+			user = form2.save()
+			user.set_password(user.password)
+			user.save()
+			ngo_group = Group.objects.get_or_create(name='NGO')
+			ngo_group[0].user_set.add(user)
+		return HttpResponseRedirect('ngo_login')
+	return render(request,"Donate/ngo_signup.html",context=dict2)
 
 def donor_signup(request):
-	if request.method == "POST":
+	form = NewDonorForm()
+	mydict={'form':form}
+	if(request.method == "POST"):
 		form = NewDonorForm(request.POST)
 		if form.is_valid():
 			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("Donate:firstpage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-		return redirect("Donate:firstpage")
-	form = NewDonorForm()
-	return render (request=request, template_name="Donate/donor_signup.html", context={"NewDonorForm":NewDonorForm})
+			user.set_password(user.password)
+			user.save()
+			donor_group = Group.objects.get_or_create(name='DONOR')
+			donor_group[0].user_set.add(user)
+		return HttpResponseRedirect('donor_login')
+	return render(request,"Donate/donor_signup.html",context=mydict)
+
+
+
+
+
+def afterlogin(request):
+	if is_ngo(request.user):
+		return render(request,'Donate/ngo_afterlogin.html')
+	else:
+		return render(request,'Donate/userprofile.html')
 
 # def donor_login(request):
     
 
-def donor_login(request):
-	if request.method == "POST":
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect("Donate:userprofile")
-			# Redirect to a success page.
-		else:
-			messages.error(request, "Invalid information.")
-			return redirect("Donate:donor_login")
-	return render(request, "Donate/donor_login.html")
+# def donor_login(request):
+# 	if request.method == "POST":
+# 		username = request.POST['username']
+# 		password = request.POST['password']
+# 		user = authenticate(request, username=username, password=password)
+# 		if user is not None:
+# 			login(request, user)
+# 			return redirect("Donate:userprofile")
+# 		else:
+# 			messages.error(request, "Invalid information.")
+# 			return redirect("Donate:donor_login")
+# 	return render(request, "Donate/donor_login.html")
 
-def ngo_signup(request):
-    return render(request, "Donate/ngo_signup.html")
 
-def ngo_login(request):
-    return render(request, "Donate/ngo_login.html")
 def userprofile(request):
     return render(request, "Donate/userprofile.html")
+def ngo_afterlogin(request):
+    return render(request, "Donate/ngo_afterlogin.html")
 
 
 
