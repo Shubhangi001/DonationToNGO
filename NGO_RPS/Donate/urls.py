53ebf90a881235b07d15383350158c49aacac7db
Shubhangi001 <shubhangispc@gmail.com>
2021-11-23 22:23:11 +0530

    urls

--- NGO_RPS/Donate/urls.py
+++ NGO_RPS/Donate/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from django.contrib.auth.views import LoginView,LogoutView
+
 app_name='Donate'
 urlpatterns = [
         path('', views.firstpage,name='firstpage'),
+        path('accounts/',include('django.contrib.auth.urls') ),
         path('userprofile', views.userprofile,name='userprofile'),
         path('donor_signup', views.donor_signup,name='donor_signup'),
-        path('donor_login', views.donor_login,name='donor_login'),
         path('ngo_signup', views.ngo_signup,name='ngo_signup'),
-        path('ngo_login', views.ngo_login,name='ngo_login'),
+        # path('donor_login', views.donor_login,name='donor_login'),
+        path('donor_login', LoginView.as_view(),name='donor_login'),
+        path('afterlogin', views.afterlogin),
+        path('ngo_login', LoginView.as_view(),name='ngo_login'),
+        path('ngo_afterlogin', views.ngo_afterlogin,name='ngo_afterlogin'),
 
 ]
